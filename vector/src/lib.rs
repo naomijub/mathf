ff7ee59404d1778017f791cc6cb115863a3724e2
Julia Naomi <jnaomi@thoughtworks.com>
2017-07-21 23:11:22 -0300

    implements basic vectors

--- vector/src/lib.rs
+++ vector/src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops;
+
 #[derive(Debug)]
 pub struct Vector2 {
     x: f32,
@@ -11,6 +13,34 @@ impl Vector2 {
 
     pub fn UP() -> Vector2 {
         Vector2 {x: 0f32, y: 1f32}
+    }
+
+    pub fn DOWN() -> Vector2 {
+        Vector2 {x: 0f32, y: -1f32}
+    }
+
+    pub fn RIGHT() -> Vector2 {
+        Vector2 {x: 1f32, y: 0f32}
+    }
+
+    pub fn LEFT() -> Vector2 {
+        Vector2 {x: -1f32, y: 0f32}
+    }
+
+    pub fn ONE() -> Vector2 {
+        Vector2 {x: 1f32, y: 1f32}
+    }
+
+    pub fn ZERO() -> Vector2 {
+        Vector2 {x: 0f32, y: 0f32}
+    }
+}
+
+impl ops::Add for Vector2 {
+    type Output = Vector2;
+
+    fn add(self, new_vec: Vector2) -> Vector2 {
+        Vector2 {x: self.x + new_vec.x, y: self.y + new_vec.y}
     }
 }
 
@@ -32,5 +62,18 @@ mod tests {
         let actual = Vector2::UP();
         assert!(actual.x == 0f32 &&
             actual.y == 1f32);
+    }
+
+    #[test]
+    fn adds_right_and_left_vectors() {
+         let actual = Vector2::RIGHT() + Vector2::LEFT();
+         assert_eq!(actual.x, 0f32);
+    }
+
+    #[test]
+    fn adds_right_and_up() {
+        let actual = Vector2::RIGHT() + Vector2::UP();
+        assert_eq!(actual.x, 1f32);
+        assert_eq!(actual.y, 1f32);
     }
 }
