65f37dd72d7a49e895f936179a954c27cb9bab85
Julia Naomi <jnaomi@thoughtworks.com>
2017-07-22 12:42:59 -0300

    Moved lib to mathf and added docs

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Math basics for a game frameworks and engines.
 
 # Inspired by:
 * Eric Lengyel, Mathmatics for 3D Game Programming and Computer Graphics, Third edition, Cengage Learning.
-* Ivan de Camargo, Geometria Analítica - Umtratamento Vetorial, Third Edition, Pearson
+* Ivan de Camargo, Geometria Analítica - Um tratamento Vetorial, Third Edition, Pearson
--- src/vector2.rs
+++ src/vector2.rs
@@ -1,5 +1,6 @@
 use std::ops;
 
+///A 2D Vector with x and y coordinates: Vector2
 #[derive(PartialEq, Debug)]
 pub struct Vector2 {
     x: f32,
@@ -7,30 +8,37 @@ pub struct Vector2 {
 }
 
 impl Vector2 {
+    ///Instantiates a new vector with to be defined values of x and y;
     pub fn new(x: f32, y: f32) -> Vector2 {
         Vector2 {x: x, y: y}
     }
 
+    ///Defines a Vector with UP direction (y=1, x=0)
     pub fn UP() -> Vector2 {
         Vector2 {x: 0f32, y: 1f32}
     }
 
+    ///Defines a Vector with DOWN direction (y=-1, x=0)
     pub fn DOWN() -> Vector2 {
         Vector2 {x: 0f32, y: -1f32}
     }
 
+    ///Defines a Vector with RIGHT direction (y=0, x=1)
     pub fn RIGHT() -> Vector2 {
         Vector2 {x: 1f32, y: 0f32}
     }
 
+    ///Defines a Vector with LEFT direction (y=0, x=-1)
     pub fn LEFT() -> Vector2 {
         Vector2 {x: -1f32, y: 0f32}
     }
 
+    ///Defines a 2D Vector with x=1 and y=1
     pub fn ONE() -> Vector2 {
         Vector2 {x: 1f32, y: 1f32}
     }
 
+    ///Defines a Modulus ZERO Vector (x=0, y=0)
     pub fn ZERO() -> Vector2 {
         Vector2 {x: 0f32, y: 0f32}
     }
@@ -39,6 +47,7 @@ impl Vector2 {
 impl ops::Add for Vector2 {
     type Output = Vector2;
 
+    ///Implements the Vector2 '+' trait
     fn add(self, new_vec: Vector2) -> Vector2 {
         Vector2 {x: self.x + new_vec.x, y: self.y + new_vec.y}
     }
@@ -47,6 +56,8 @@ impl ops::Add for Vector2 {
 impl ops::Mul<f32> for Vector2 {
     type Output = Vector2;
 
+    ///Implements The scalar multiplication of a Vector2 with a f32. Other numbers should
+    ///be passed with 'i as f32'
     fn mul(self, value: f32) -> Vector2 {
         Vector2 {x: self.x * value, y: self.y * value}
     }
